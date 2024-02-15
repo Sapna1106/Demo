d069a8f798859845c7082a65b3e160890b4ff962
Aayushi Bhatiya <aayushi.bhatiya@xebia.com>
2024-02-15 11:01:27 +0530

    patch3

--- src/main/java/com/example/demo/repository/CourseRepository.java
+++ src/main/java/com/example/demo/repository/CourseRepository.java
@@ -1,9 +1,9 @@
-//package com.example.demo.repository;
-//
-//import com.example.demo.entity.Course;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//
-//public interface CourseRepository extends JpaRepository<Course, Long> {
-//}
+package com.example.demo.repository;
+
+import com.example.demo.entity.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+public interface CourseRepository extends JpaRepository<Course, Long> {
+}
